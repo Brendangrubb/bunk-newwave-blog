b22e29a529333c4489d1b1ea0853c2406162633f
brendan grubb and charles <brendan+charles@>
2017-03-23 16:50:18 -0700

    well it's just all fucked up now

---
+++ app/components/search-tile.js
@@ -0,0 +1,16 @@
+// import Ember from 'ember';
+//
+// export default Ember.Component.extend({
+//   openSearch: false,
+//   actions: {
+//     showSearch() {
+//       this.set('openSearch', true);
+//     },
+//     searchByAuthor() {
+//       var params = {
+//         searchByAuthor: this.get('searchByAuthor'),
+//       }
+//       this.sendAction('searchByAuthor', params);
+//     },
+//   }
+// });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,32 @@
 import Ember from 'ember';
+// import InfinityRoute from "ember-infinity/mixins/route";
+
+
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('article');
+  model(params) {
+    var article = this.store.findAll('article');
+    // console.log(article);
+    return article;
   }
+  // actions: {
+  //   searchByAuthor(param){
+  //     var newauthors = this.store.query('article', { filter: { author: param.searchByAuthor } })
+  //     .then(function(authors) {
+  //       console.log(authors);
+  //     })
+  //   }
+  // }
 });
 
-// model() {
-//   return this.store.findAll('article');
-// },
+
+
+// export default Ember.Route.extend(InfinityRoute, {
+//   model() {
+//     return this.infinityModel("article", { perPage: 5, startingPage: 1 });
+//   }
+// });
+//ember documentation on query
+// this.store.query('person', { filter: { name: 'Peter' } }).then(function(peters) {
+//   // Do something with `peters`
+// });
